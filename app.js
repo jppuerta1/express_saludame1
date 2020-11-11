3793c00fc24f5857836c51bd0b7aabfd76d5fae7
jppuerta1 <jppuertaorrego@gmail.com>
2020-11-10 20:20:02 -0800

    segundo commit

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express ()
 
 app.get('/', (req,res)=>{
     const nombre = req.query.nombre;
-    if(nombre==='Juan'){
-        res.send(`<h1>Hola ${nombre}!</h1>`)
-    }else{res.send('<h1>Hola desconocido!</h1>')}
+    if(nombre===''||nombre===undefined){
+        res.send('<h1>Hola desconocido!</h1>')
+    }else{res.send(`<h1>Hola ${nombre}!</h1>`)}
     
 })
 app.listen (3000,()=>console.log('listening on port 3000'))
